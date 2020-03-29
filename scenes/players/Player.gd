373ab543eb9faeda25fbdd6d98c4baf7e0d4158f
Lester Han <lester.han@gmail.com>
2020-03-29 12:54:25 -0400

    add thrusters to players

--- scenes/players/Player.gd
+++ scenes/players/Player.gd
@@ -18,30 +18,53 @@ onready var texture = ($Ship as Sprite).texture
 
 
 func _process(delta):
+	var fire_thruster_map = {
+		"forward": false,
+		"reverse": false,
+		"port": false,
+		"starboard": false,
+	}
 	var player_name = "player" + playerIdx
 	if Input.is_action_pressed(player_name + "_shoot"):
 		shoot()
-
+	
 	if Input.is_action_pressed(player_name + "_left"):
 		rot -= rot_speed * delta
+		fire_thruster_map["starboard"] = true # fires opposite thruster
 	if Input.is_action_pressed(player_name + "_right"):
 		rot += rot_speed * delta
+		fire_thruster_map["port"] = true # fires opposite thruster
 
 	rotation = rot
 	var thrust_direction = 0
 	if Input.is_action_pressed(player_name + "_thrust"):
 		thrust_direction = 1
+		fire_thruster_map["forward"] = true
 	elif Input.is_action_pressed(player_name + "_reverse_thrust"):
 		thrust_direction = -1
+		fire_thruster_map["reverse"] = true
 	var acc = get_acceleration(thrust_direction)
 	vel += acc * delta
 	position = wrap_screen_edges(position + vel * delta)
+	fire_thrusters(fire_thruster_map)
 
 func get_acceleration(thrust_direction: int):
 	var friction_force = vel * friction
 	var applied_thrust = thrust * thrust_direction
 	return Vector2(applied_thrust, 0).rotated(rotation - PI / 2) - friction_force
 
+func fire_thrusters(fire_thruster_map: Dictionary):
+	var particle_to_thruster_map = {
+		"forward": $ThrusterParticles/ForwardThrusterParticle,
+		"reverse": $ThrusterParticles/ReverseThrusterParticle,
+		"port": $ThrusterParticles/PortThrusterParticle,
+		"starboard": $ThrusterParticles/StarboardThrusterParticle,
+	}
+	for thruster in fire_thruster_map:
+		particle_to_thruster_map.get(thruster).emitting = fire_thruster_map.get(thruster)
+
+func process_forward_thruster():
+	$thrusterParticle.emitting = true
 
 func wrap_screen_edges(pos):
 	var new_pos = pos
