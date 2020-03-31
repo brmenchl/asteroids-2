d359e2bcd9ebd0b8271f15ac29240851562aafa9
Brad M <brmenchl@gmail.com>
2020-03-30 22:51:17 -0400

    clean up

--- scenes/cowboy/Cowboy.gd
+++ scenes/cowboy/Cowboy.gd
@@ -1,29 +1,44 @@
 extends RigidBody2D
 
-export var rotation_speed := 50
+class_name Cowboy
+
+export var spin_thrust = 100
+export var max_speed = 280
+export var playerIdx = ''
 export var bullet_force := 500
+export var shots_per_second := 3
+var player_name = "player" + playerIdx
+var rotation_dir = 0
 
-func turn_left(delta: float):
-	#rotation -= rotation_speed * delta
-	apply_torque_impulse(-rotation_speed)
+func _ready():
+	$AnimatedSprite.speed_scale = shots_per_second * 4
 
-
-func turn_right(delta: float):
-	#rotation += rotation_speed * delta
-	apply_torque_impulse(rotation_speed)
+func process_input():
+	if Input.is_action_pressed(player_name + "_shoot"):
+		shoot()
 	
+	rotation_dir = 0
+	if Input.is_action_pressed(player_name + "_left"):
+		rotation_dir -= 1
+	if Input.is_action_pressed(player_name + "_right"):
+		rotation_dir += 1
+
+func _process(_delta):
+	process_input()
+
+func _physics_process(_delta):
+	apply_torque_impulse(rotation_dir * spin_thrust)
+
+func _integrate_forces(state):
+	if state.linear_velocity.length()>max_speed:
+		state.linear_velocity=state.linear_velocity.normalized()*max_speed
+
 func shoot():
 	if !$AnimatedSprite.is_playing():
 		var shot_impulse = -Vector2(bullet_force, 0).rotated(rotation)
 		apply_central_impulse(shot_impulse)
 		$AnimatedSprite.animation = "shoot"
 		$AnimatedSprite.play()
-
-func thrust(_delta):
-	pass
-
-func _physics_process(delta):
-	$Node.process_check_input(delta, self)
 
 func _on_AnimatedSprite_animation_finished():
 	$AnimatedSprite.stop()
