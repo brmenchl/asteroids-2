df33c3f4221d4e80ecc263d22cf0b8003db18f9f
Brad M <brmenchl@gmail.com>
2020-03-31 19:21:06 -0400

    quit with esc

--- scenes/Main.gd
+++ scenes/Main.gd
@@ -3,3 +3,7 @@ extends Node
 
 func _ready():
 	$HUD.init_with_players([$Players/Player1, $Players/Player2])
+
+func _process(delta: float) -> void:
+	if OS.is_debug_build() and Input.is_action_pressed("debug_quit"):
+		get_tree().quit()
