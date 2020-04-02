ab3b9708b98fcd7d94f37288837ed24b1468708d
Lester Han <lester.han@gmail.com>
2020-04-02 10:31:09 -0400

    strictly wrapping on one edge works

--- scenes/asteroids/Asteroid.gd
+++ scenes/asteroids/Asteroid.gd
@@ -1,24 +1,29 @@
-class_name Asteroid
+class_name Asteroid	
 extends RigidBody2D
 
 export (PackedScene) var hit_effect
+onready var screensize := get_viewport().get_visible_rect().size
 var asteroid_scene = load("res://scenes/asteroids/Asteroid.tscn")
-var decoupler_node: Node
+onready var decoupler_node = Node.new()
 var is_ghost: bool
 var ghost_promoted: bool
+var ghost_wrap_direction
 var ghost
-onready var screensize := get_viewport().get_visible_rect().size
 
+enum WRAP_DIRECTION {
+	TOP,
+	LEFT,
+	RIGHT,
+	BOTTOM,
+	HORIZONTAL,
+	VERTICAL
+}
 
 func _ready():
 	add_to_group('asteroid')
-	decoupler_node = Node.new()
-	get_tree().get_root().get_node("Main/ScreenWrapperAreas/WrapArea").connect(
-		"body_entered", self, "_on_WrapArea_body_entered", [], Object.CONNECT_ONESHOT
-	)
-	get_tree().get_root().get_node("Main/ScreenWrapperAreas/PlayArea").connect(
-		"body_exited", self, "_on_PlayArea_body_exited", [], Object.CONNECT_ONESHOT
-	)
+	get_tree().get_root().get_node("Main/ScreenWrapperAreas/VerticalWrapArea").connect("body_entered", self, "_on_VerticalWrapArea_body_entered")
+	get_tree().get_root().get_node("Main/ScreenWrapperAreas/HorizontalWrapArea").connect("body_entered", self, "_on_HorizontalWrapArea_body_entered")
+	get_tree().get_root().get_node("Main/ScreenWrapperAreas/PlayArea").connect("body_exited", self, "_on_PlayArea_body_exited")
 
 
 func hit_by_bullet(position, rotation, _damage):
@@ -28,44 +33,73 @@ func hit_by_bullet(position, rotation, _damage):
 	self.get_parent().add_child(hit_fx)
 
 
+func wrap_direction_from_transform(transform):
+	var horizontal_middle_of_screen = screensize.x / 2
+	var vertical_middle_of_screen = screensize.y / 2
+	match ghost_wrap_direction:
+		WRAP_DIRECTION.VERTICAL:
+			if transform.origin.y >= vertical_middle_of_screen:
+				return WRAP_DIRECTION.TOP
+			elif transform.origin.y < vertical_middle_of_screen:
+				return WRAP_DIRECTION.BOTTOM
+		WRAP_DIRECTION.HORIZONTAL:
+			if transform.origin.x >= horizontal_middle_of_screen:
+				return WRAP_DIRECTION.RIGHT
+			elif transform.origin.x < horizontal_middle_of_screen:
+				return WRAP_DIRECTION.LEFT		
+
+func ghost_transform_from_parent(parent_transform):
+	var ghost_transform = parent_transform
+	match wrap_direction_from_transform(parent_transform):
+		WRAP_DIRECTION.TOP:
+			ghost_transform.origin.y = parent_transform.origin.y - screensize.y
+		WRAP_DIRECTION.BOTTOM:
+			ghost_transform.origin.y = parent_transform.origin.y + screensize.y
+		WRAP_DIRECTION.LEFT:
+			ghost_transform.origin.x = parent_transform.origin.x + screensize.x
+		WRAP_DIRECTION.RIGHT:
+			ghost_transform.origin.x = parent_transform.origin.x - screensize.x
+	return ghost_transform 
+	
+
 func _integrate_forces(state):
 	if is_ghost:
 		var original_asteroid = get_parent().get_parent()
-		var ghost_transform = original_asteroid.get_transform()
-		ghost_transform.origin.y = original_asteroid.get_transform().origin.y + screensize.y
-		state.set_transform(ghost_transform)
+		state.set_transform(ghost_transform_from_parent(original_asteroid.get_transform()))
 		state.linear_velocity = original_asteroid.linear_velocity
 		state.angular_velocity = original_asteroid.angular_velocity
 
 
-func spawn_ghost():
+func spawn_ghost(wrap_orientation):
 	ghost = asteroid_scene.instance()
 	ghost.is_ghost = true
-	ghost.position = position + screensize
-	ghost.name = get_name() + "-jr"
+	ghost.position = position + screensize + screensize # spawns off screen for 1 frame before physics take over
+	ghost.ghost_wrap_direction = wrap_orientation
 	decoupler_node.add_child(ghost)
 	add_child(decoupler_node)
+	
+
+func _on_VerticalWrapArea_body_entered(body):
+	if body.is_in_group("asteroid") && !body.is_ghost && !body.ghost:
+		body.call_deferred("spawn_ghost", WRAP_DIRECTION.VERTICAL)
 
 
-func _on_WrapArea_body_entered(body):
-	if body.is_in_group("asteroid") && ! body.ghost:
-		print(body.get_name() + " is going to spawn " + body.get_name() + "-jr")
-		body.call_deferred("spawn_ghost")
+func _on_HorizontalWrapArea_body_entered(body):
+	if body.is_in_group("asteroid") && !body.is_ghost && !body.ghost:
+		body.call_deferred("spawn_ghost", WRAP_DIRECTION.HORIZONTAL)
 
 
 func promote_ghost():
-	if ! ghost_promoted && ghost:
+	if !ghost_promoted && ghost:
 		var parent = get_parent()
 		decoupler_node.remove_child(ghost)
 		ghost.is_ghost = false
 		parent.add_child(ghost)
 		ghost_promoted = true
-		print("promoted!")
 
 
 func _on_PlayArea_body_exited(body):
 	# promote ghost to be a sibling
-	if body.is_in_group("asteroid") && ! body.is_ghost:
-		print(body.get_name() + " is going to promote " + body.ghost.get_name())
+	if body.is_in_group("asteroid") && !body.is_ghost:
 		body.call_deferred("promote_ghost")
 		body.queue_free()
