e212d8fc01b610062be620dc37b38def490baea3
Lester Han <lester.han@gmail.com>
2020-04-03 16:15:27 -0400

    asteroid now wraps once

--- scenes/asteroids/Asteroid.gd
+++ scenes/asteroids/Asteroid.gd
@@ -4,18 +4,38 @@ extends RigidBody2D
 export (PackedScene) var hit_effect
 onready var screensize := get_viewport().get_visible_rect().size
 
-func _ready():
-	var new_sprite = $Sprite.duplicate(Node.DUPLICATE_USE_INSTANCING)
-	new_sprite.position.x = position.x + 500
-	var new_collision = $CollisionPolygon2D.duplicate(DUPLICATE_USE_INSTANCING)
-	new_collision.position.x = position.x + 500
+func add_new_instance(position_offset: Vector2):
+	var new_sprite = $AsteroidSprite.duplicate(DUPLICATE_USE_INSTANCING)
+	var new_collision = $AsteroidCollision.duplicate(DUPLICATE_USE_INSTANCING)
+	new_sprite.position += position_offset
+	new_collision.position += position_offset
 	add_child(new_sprite)
 	add_child(new_collision)
+
+func _ready():
+	# screenwrap offset sections:
+	# 0 |     1    | 2
+	# 7 | playarea | 3
+	# 6 |     5    | 4
+	
+	var screenwrap_clone_offsets = [
+		Vector2(-1 * screensize.x, -1 * screensize.y), # 0
+		Vector2(0, -1 * screensize.y), # 1
+		Vector2(screensize.x, -1 * screensize.y), # 2
+		Vector2(screensize.x, 0), # 3
+		Vector2(screensize.x, screensize.y), # 4
+		Vector2(0, screensize.y), # 5
+		Vector2(-1 * screensize.x, screensize.y), # 6
+		Vector2(-1 * screensize.x, 0), # 7
+	]
+	
+	for offset in screenwrap_clone_offsets:
+		add_new_instance(offset)
 	
 func _process(delta):
 	var rotation_per_frame = delta / 5
-	$Sprite.rotation += rotation_per_frame
-	$CollisionPolygon2D.rotation += rotation_per_frame
+	for node in get_children():
+		node.rotation += rotation_per_frame
 
 func hit_by_bullet(position, rotation, _damage):
 	var hit_fx = hit_effect.instance()
