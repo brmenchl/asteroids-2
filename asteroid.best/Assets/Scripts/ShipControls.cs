afd4b090f5c523083e8a6480db36e66eb402a407
Lester Han <lester.han@gmail.com>
2020-04-06 23:24:07 -0400

    add firing behaviour

--- asteroid.best/Assets/Scripts/ShipControls.cs
+++ asteroid.best/Assets/Scripts/ShipControls.cs
@@ -6,9 +6,11 @@ using UnityEngine;
 public class ShipControls : MonoBehaviour
 {
     public Rigidbody shipRigidBody;
+    public Rigidbody bullet;
     public float afterburnerPower = 35000;
     public float cruisePower = 22000;
-    public float torque = 29;
+    public float torque = 30;
+    public float bulletVelocity = 600;
     
     private float _thrustInput;
     private float _torqueInput;
@@ -24,6 +26,11 @@ public class ShipControls : MonoBehaviour
     {
         _thrustInput = Input.GetAxis("Vertical");
         _torqueInput = Input.GetAxis("Horizontal");
+        if (Input.GetButtonDown("Fire1"))
+        {
+            FireBullet();
+            print("supsupsup");
+        }
     }
 
     private void FixedUpdate()
@@ -32,5 +39,11 @@ public class ShipControls : MonoBehaviour
         shipRigidBody.AddRelativeTorque(Vector3.up * (_torqueInput * torque));
         shipRigidBody.AddRelativeForce(Vector3.forward * (_thrustInput * cruisePower));
     }
-    
+
+    void FireBullet()
+    {
+        var myTransform = transform;
+        Rigidbody bulletInstance = Instantiate(this.bullet, myTransform.position, myTransform.rotation);
+        bulletInstance.velocity = transform.forward * bulletVelocity;
+    }
 }
