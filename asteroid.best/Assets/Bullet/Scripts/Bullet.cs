193e73e0d1fe179e3ab445731d55fdf7f907e4f9
Brad Menchl <brmenchl@gmail.com>
2020-04-13 12:15:16 -0400

    Improve bullets

--- asteroid.best/Assets/Bullet/Scripts/Bullet.cs
+++ asteroid.best/Assets/Bullet/Scripts/Bullet.cs
@@ -2,14 +2,13 @@ using System;
 using UnityEngine;
 using Zenject;
 
-public class Bullet : MonoBehaviour, IPoolable<Vector3, Quaternion, Vector3, IMemoryPool>, IDisposable
+public class Bullet : MonoBehaviour, IPoolable<Transform, float, IMemoryPool>
 {
     IMemoryPool _pool;
-    
-    public void Dispose()
-    {
-        _pool.Despawn(this);
-    }
+    float _startTime;
+
+    [Inject]
+    readonly Settings _settings;
 
     public void OnDespawned()
     {
@@ -17,13 +16,40 @@ public class Bullet : MonoBehaviour, IPoolable<Vector3, Quaternion, Vector3, IMe
         GetComponent<Rigidbody>().velocity = Vector3.zero;
     }
 
-    public void OnSpawned(Vector3 position, Quaternion rotation, Vector3 velocity, IMemoryPool pool)
+    public void OnSpawned(Transform trans, float spawnOffset, IMemoryPool pool)
     {
         _pool = pool;
-        transform.position = position;
-        transform.rotation = rotation;
-        GetComponent<Rigidbody>().velocity = velocity;
+        transform.SetPositionAndRotation(trans.position + (trans.forward * spawnOffset), trans.rotation);
+        GetComponent<Rigidbody>().velocity = transform.forward * _settings.Speed;
+        _startTime = Time.realtimeSinceStartup;
     }
 
-    public class Factory : PlaceholderFactory<Vector3, Quaternion, Vector3, Bullet> { }
+    private void Update()
+    {
+        if (Time.realtimeSinceStartup - _startTime > _settings.LifeTime)
+        {
+            _pool.Despawn(this);
+        }
+    }
+
+    public void OnTriggerEnter(Collider other)
+    {
+        var player = other.GetComponent<ShipControls>();
+
+        if (player != null)
+        {
+            player.TakeDamage(_settings.Damage);
+            _pool.Despawn(this);
+        }
+    }
+
+    public class Factory : PlaceholderFactory<Transform, float, Bullet> { }
+
+    [Serializable]
+    public class Settings
+    {
+        public float Speed;
+        public float LifeTime;
+        public float Damage;
+    }
 }
